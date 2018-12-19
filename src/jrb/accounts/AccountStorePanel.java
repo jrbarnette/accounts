3f5414a8cb8f054579248adbfe9c0e79ec14f188
Richard Barnette <rbarnette@acm.org>
2018-12-19 13:11:35 -0800

    Move UI layout creation to a private method.

--- src/jrb/accounts/AccountStorePanel.java
+++ src/jrb/accounts/AccountStorePanel.java
@@ -22,6 +22,44 @@ class AccountStorePanel extends JPanel {
 
     private AccountStore myAccountStore;
 
+    static private final String SEARCH = "Search";
+    static private final String UPDATE = "Update";
+
+    private JComponent createAccountDataPanel() {
+	JPanel dataPanel = new JPanel(new GridLayout(0, 3));
+
+	// 1st row - Description
+	dataPanel.add(new JLabel("Description"));
+	descriptionText = new JTextField();
+	dataPanel.add(descriptionText);
+	dataPanel.add(new JLabel(" "));
+
+	// 2nd row - URL
+	dataPanel.add(new JLabel("URL"));
+	urlText = new JTextField();
+	dataPanel.add(urlText);
+	JButton searchButton = new JButton(SEARCH);
+	dataPanel.add(searchButton);
+
+	// 3rd row - User name
+	dataPanel.add(new JLabel("User Name"));
+	usernameText = new JTextField();
+	dataPanel.add(usernameText);
+	dataPanel.add(new JLabel(" "));
+
+	// 4th row - Password
+	dataPanel.add(new JLabel("Password"));
+	passwordText = new JPasswordField();
+	dataPanel.add(passwordText);
+	dataPanel.add(new JButton("Copy"));
+
+	// 5th row - Buttons
+	dataPanel.add(new JButton(UPDATE));
+	dataPanel.add(new JButton("Generate ..."));
+
+	return dataPanel;
+    }
+
     /**
      */
     public AccountStorePanel() {
@@ -35,36 +73,6 @@ class AccountStorePanel extends JPanel {
 	aScrollPane.setViewportView(accountList);
 	add(aScrollPane);
 
-	JPanel accountPanel = new JPanel(new GridLayout(0, 3)); {
-	    JPanel subPanel;
-	    // 1st row - Description
-	    accountPanel.add(new JLabel("Description"));
-	    descriptionText = new JTextField();
-	    accountPanel.add(descriptionText);
-	    accountPanel.add(new JButton("Search"));
-
-	    // 2nd row - URL
-	    accountPanel.add(new JLabel("URL"));
-	    urlText = new JTextField();
-	    accountPanel.add(urlText);
-	    accountPanel.add(new JButton("Search"));
-
-	    // 3rd row - User name
-	    accountPanel.add(new JLabel("User Name"));
-	    usernameText = new JTextField();
-	    accountPanel.add(usernameText);
-	    accountPanel.add(new JLabel(" "));
-
-	    // 4th row - Password
-	    accountPanel.add(new JLabel("Password"));
-	    passwordText = new JPasswordField();
-	    accountPanel.add(passwordText);
-	    accountPanel.add(new JButton("Copy"));
-
-	    // 5th row - Buttons
-	    accountPanel.add(new JButton("Update"));
-	    accountPanel.add(new JButton("Generate ..."));
-	}
-	add(accountPanel);
+	add(createAccountDataPanel());
     }
 }
