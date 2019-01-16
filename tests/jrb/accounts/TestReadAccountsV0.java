636529c5c24956b692b6aef3ad283f3431398b41
Richard Barnette <rbarnette@acm.org>
2019-01-15 21:12:40 -0800

    Add a test against a canned data file.
    This will enables testing against old input files when the file
    format changes.

---
+++ tests/jrb/accounts/TestReadAccountsV0.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019, by J. Richard Barnette.  All Rights Reserved.
+ */
+
+package jrb.accounts;
+
+import org.junit.Test;
+
+public class TestReadAccountsV0 extends AccountStoreFactory {
+    private static final Account[] TEST_ACCOUNTS = {
+	new Account("xxx desc", "http://c.com", "u0", "p3"),
+	new Account("yyy desc", "http://b.com", "u0", "p2"),
+	new Account("zzz desc", "http://a.com", "u0", "p0"),
+    };
+
+    private static final String TEST_RESOURCE = "test-data/v0-sample.accts";
+
+    public TestReadAccountsV0() {
+	super(TEST_ACCOUNTS);
+    }
+
+    @Test
+    public void testSampleFile() {
+	validateResource(TEST_RESOURCE);
+    }
+}
