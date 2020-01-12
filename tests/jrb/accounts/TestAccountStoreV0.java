347c05c88f4b4449adf522f160484d459d78c6c9
Richard Barnette <rbarnette@acm.org>
2020-01-11 17:49:18 -0800

    Rename "TestReadAccountsV?" to "TestAccountStoreV?"

--- tests/jrb/accounts/TestAccountStoreV0.java
+++ tests/jrb/accounts/TestAccountStoreV0.java
@@ -4,22 +4,25 @@
 
 package jrb.accounts;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.security.GeneralSecurityException;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class TestReadAccountsV1 extends AccountStoreSupport {
+public class TestAccountStoreV0 extends AccountStoreSupport {
     private static final Account[] TEST_ACCOUNTS = {
 	new Account("xxx desc", "http://c.com", "u0", "p3"),
 	new Account("yyy desc", "http://b.com", "u0", "p2"),
 	new Account("zzz desc", "http://a.com", "u0", "p0"),
     };
 
-    private static final String TEST_RESOURCE = "test-data/v1-sample.accts";
+    private static final String TEST_RESOURCE = "test-data/v0-sample.accts";
 
-    public TestReadAccountsV1() {
+    public TestAccountStoreV0() {
 	super(TEST_ACCOUNTS);
     }
 
@@ -28,5 +31,14 @@ public class TestReadAccountsV1 extends AccountStoreSupport {
 	    throws IOException, GeneralSecurityException {
 	AccountStore accounts = createFromResource(TEST_RESOURCE);
 	validateContent(accounts);
+    }
+
+    @Test
+    public void testConversion()
+	    throws IOException, GeneralSecurityException {
+	File tFile = File.createTempFile("test", ".acct");
+	AccountStore accounts = createFromResource(TEST_RESOURCE);
+	accounts.writeAccounts(new FileOutputStream(tFile));
+	validateContent(createFromFile(tFile));
     }
 }
--- tests/jrb/accounts/TestAccountStoreV1.java
+++ tests/jrb/accounts/TestAccountStoreV1.java
@@ -4,25 +4,22 @@
 
 package jrb.accounts;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.security.GeneralSecurityException;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-public class TestReadAccountsV0 extends AccountStoreSupport {
+public class TestAccountStoreV1 extends AccountStoreSupport {
     private static final Account[] TEST_ACCOUNTS = {
 	new Account("xxx desc", "http://c.com", "u0", "p3"),
 	new Account("yyy desc", "http://b.com", "u0", "p2"),
 	new Account("zzz desc", "http://a.com", "u0", "p0"),
     };
 
-    private static final String TEST_RESOURCE = "test-data/v0-sample.accts";
+    private static final String TEST_RESOURCE = "test-data/v1-sample.accts";
 
-    public TestReadAccountsV0() {
+    public TestAccountStoreV1() {
 	super(TEST_ACCOUNTS);
     }
 
@@ -31,14 +28,5 @@ public class TestReadAccountsV0 extends AccountStoreSupport {
 	    throws IOException, GeneralSecurityException {
 	AccountStore accounts = createFromResource(TEST_RESOURCE);
 	validateContent(accounts);
-    }
-
-    @Test
-    public void testConversion()
-	    throws IOException, GeneralSecurityException {
-	File tFile = File.createTempFile("test", ".acct");
-	AccountStore accounts = createFromResource(TEST_RESOURCE);
-	accounts.writeAccounts(new FileOutputStream(tFile));
-	validateContent(createFromFile(tFile));
     }
 }
